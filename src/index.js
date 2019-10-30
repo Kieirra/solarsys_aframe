80701cbc1ed02d5ff266081dc9bb17de77049cc9
Luc MARONGIU <velith.it@gmail.com>
2019-10-30 16:29:45 +0100

    Add register Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import aframe from 'aframe';
+
+aframe.registerComponent('solar-system', {
+    init: function () {
+        // HERE YOUR INITIALISATION CODE
+        console.log("Hello !");
+    },
+    tick: function (time) {
+        // HERE YOUR ON EACH FRAME
+    }
+});
